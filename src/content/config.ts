fde4b9d4d7e8b1b80736893ec85077e4d8f61ddc
Marla Lain <marla@powr.run>
2024-02-03 05:45:34 -0300

    chore: moves to microblog style

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import {defineCollection, z} from 'astro:content';
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    id: z.string().default(() => crypto.randomUUID()),
+    content: z.string(),
+    image: z.string().optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
 });
 
-export const collections = { blog };
+export const collections = {blog};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const {addDynamicIconSelectors} = require('@iconify/tailwind');
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    addDynamicIconSelectors(),
+  ],
 }
 
