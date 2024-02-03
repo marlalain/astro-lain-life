e9c2b785f0ec32aef4af1cbc976d7a6c431996bc
Marla Lain <marla@powr.run>
2024-02-03 06:00:05 -0300

    fix: adds deno to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import deno from '@astrojs/deno'
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: deno(),
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
