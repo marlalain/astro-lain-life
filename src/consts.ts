e6d6f2b3f2d3c3ad7e917d3382720e512baef45e
Marla Lain <marla@powr.run>
2024-02-04 08:40:39 -0300

    feat: pronouns

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "lain's life";
+export const SITE_TITLE = "marla lain";
+export const PRONOUNS = "she/her"
 export const SITE_DESCRIPTION = 'factory owner, business hater, things sayer';
