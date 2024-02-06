4581c42a8414703b5186c05818ccd3d423fe6d8c
Marla Lain <marla@powr.run>
2024-02-06 11:32:05 -0300

    fix(astro): adds rss

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,6 @@
-import {defineCollection, z} from 'astro:content';
-
-const blog = defineCollection({
-  type: 'content',
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    id: z.string().default(() => crypto.randomUUID()),
-    content: z.string(),
-    image: z.string().optional(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-  }),
-});
-
-export const collections = {blog};
+export type Post = {
+  id: string
+  content: string
+  postedAt: string
+  imageURL?: string
+}
